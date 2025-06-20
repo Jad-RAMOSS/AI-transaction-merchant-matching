a76c51ca731da14f098a9857211dd77dec16db5c
Nasser Mohamed <nasser.mohamed.eid.1@gmail.com>
2025-06-20 21:33:23 +0300

    clean data

--- clean_data.py
+++ clean_data.py
@@ -1,20 +1,25 @@
 import pandas as pd 
-import numpy as np
-import re
-data_path='data/combined_output.xlsx'
+# import numpy as np
+# import re
+data_path='D:/projects/Transaction-Matching/data/combined_output.xlsx'
 def clean_data(data_path):
     data=pd.read_excel(data_path)
     ##DROP INSTA AND NULL
-    data = data[~data['Description'].str.contains('IPN', na=False)]
-    data.dropna(inplace=True)
+    data = data[data['Description'].apply(lambda x: isinstance(x, str))]
+    data = data[~data['Description'].str.contains('IPN', case=False)]
+    
+    data.dropna(subset=['Description', 'Rep'], inplace=True)
+
 
     ## Rep
-    data['Rep'] =data['Rep'].str.lower()
+    # data['Rep'] =data['Rep'].str.lower()
+    # data['Rep'] = data['Rep'].str.lower().str.strip()
+    data['Rep'] = data['Rep'].str.lower().str.replace(" ", "", regex=False)
 
     ## Description
     data['Description'] = data['Description'].str.replace(r'^.*?(?:\|| {3,})', '', regex=True)
 
-    # data.to_excel('D:/projects/Transaction-Matching/data/NEW.xlsx', index=False)
+    data.to_excel('D:/projects/Transaction-Matching/data/DATA.xlsx', index=False)
     return data
 
 
