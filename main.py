042272773bec27112c1fcbdef7f651626a4e22dc
Jad ElWahy <jad.sherif5@gmail.com>
2025-06-25 15:45:37 +0300

    added save to csv and excel and export to oracle database

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import pandas as pd
 from model import call_model,predict
 from embedding import embedded,decoded
+from data_to_oracle import export_to_oracle
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.types import VARCHAR, DATE, FLOAT, INTEGER
+import urllib.parse
 
 
 def main():
-    data_path='D:/projects/Transaction-Matching/data/NEWtest2.xlsx'
-    model_path="D:/projects/Transaction-Matching/saved model"
-    encoder_path="D:/projects/Transaction-Matching/saved model/label_encoder.pkl"
+    data_path='C:\\AI Merchant Transaction Matching\\LLM\\Transaction-Matching\\data\\Test\\NBE & BM 24-6-2025.xlsx'
+    model_path="C:\\AI Merchant Transaction Matching\\LLM\\Transaction-Matching\\saved model"
+    encoder_path="C:\\AI Merchant Transaction Matching\\LLM\\Transaction-Matching\\saved model\\label_encoder.pkl"
 
     model,tokenizer,le =call_model(model_path,encoder_path)
     print("================================== model succcesss =====================================")
-    data_loader=embedded(data_path,tokenizer)
+    data_loader, test_data = embedded(data_path,tokenizer)
     print("================================== data_loader succcesss =====================================")
     all_texts,all_preds,all_dates,all_amounts=predict(model,data_loader)
     print("================================== predict succcesss =====================================")
-    result=decoded(all_texts,all_preds,all_dates,all_amounts,le)
+    result=decoded(all_texts,all_preds,test_data,le)
     print(result)
-    result.to_excel('D:/projects/Transaction-Matching/data/predict.xlsx', index=False)
+    file_name = os.path.basename(data_path).split('.')[0]
+    # Save to Excel and CSV
+    result.to_excel(f'C:\\AI Merchant Transaction Matching\\LLM\\Transaction-Matching\\output\\prediction_{file_name}.xlsx', index=False)
+    result.to_csv(f'C:\\AI Merchant Transaction Matching\\LLM\\Transaction-Matching\\output\\prediction_{file_name}.csv', index=False)
+    
+    # Export to Oracle Database
+    export_to_oracle(result)
+
 
 
 
