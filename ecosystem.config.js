d81d176d9bfab12de5390e632f628bbaea8f159a
Jad ElWahy <jad.sherif5@gmail.com>
2025-06-29 12:44:50 +0300

    file to run the pm2 thingy using (pm2 start ecosystem.config.js)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
       name: "ai-merchant-api",
       script: "uvicorn",
-      interpreter: "python",
+      interpreter: "C:\\Users\\gelwahy\\AppData\\Local\\anaconda3\\envs\\AI_Merchant_Transaction_Matching\\python.exe",
       args: "main:app --host 0.0.0.0 --port 8000 --workers 4"
     }]
   }
