8d745e5e580bf69ab9890b55c195aa365420b202
Jad ElWahy <jad.sherif5@gmail.com>
2025-07-01 15:59:58 +0300

    changed directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ai-merchant-api',
-      cwd: '/home/jad/Bank_Statement_Rep_Prediction/AI-transaction-merchant-matching',
+      cwd: '/app/Bank_Statement_Rep_Prediction/AI-transaction-merchant-matching',
       script: 'main.py',
       args: '',
       interpreter: '/usr/bin/python3',
