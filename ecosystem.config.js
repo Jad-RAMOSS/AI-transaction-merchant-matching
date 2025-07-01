e542da1c0b2b054c597df01e5798f511636dec23
Jad ElWahy <jad.sherif5@gmail.com>
2025-07-01 11:55:25 +0300

    changed python path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       cwd: '/home/jad/Bank_Statement_Rep_Prediction/AI-transaction-merchant-matching',
       script: 'main.py',
       args: '',
-      interpreter: '/usr/bin/python /usr/bin/python2.7 /usr/bin/python3.6 /usr/bin/python3.6m /usr/lib/python2.7 /usr/lib/python3.6 /usr/lib64/python3.6 /usr/lib64/python2.7 /etc/python /usr/local/lib/python3.6 /usr/include/python2.7 /usr/include/python3.6m /usr/share/man/man1/python.1.gz',
+      interpreter: '/usr/bin/python3',
       env: { PYTHONUNBUFFERED: '1' }
     }
   ]
