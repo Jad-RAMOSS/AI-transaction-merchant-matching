d20a14234fe89dad543ff3d7b901cfc6eacce540
Jad ElWahy <jad.sherif5@gmail.com>
2025-07-01 10:07:59 +0300

    made the clean_data function take a dataframe not a data path

--- clean_data.py
+++ clean_data.py
@@ -2,24 +2,24 @@ import pandas as pd
 # import numpy as np
 # import re
 # data_path='D:/projects/Transaction-Matching/data/combined_output.xlsx'
-def clean_data(data_path):
-    data=pd.read_excel(data_path)
+def clean_data(data):
     ##DROP INSTA AND NULL
-    data = data[data['Description'].apply(lambda x: isinstance(x, str))]
-    data = data[~data['Description'].str.contains('IPN', case=False)]
+    data = data[data['description'].apply(lambda x: isinstance(x, str))]
+    data = data[~data['description'].str.contains('IPN', case=False)]
     
-    data.dropna(subset=['Description', 'Rep'], inplace=True)
+    data.dropna(subset=['description', 'rep'], inplace=True)
 
 
     ## Rep
     # data['Rep'] =data['Rep'].str.lower()
     # data['Rep'] = data['Rep'].str.lower().str.strip()
-    data['Rep'] = data['Rep'].str.lower().str.replace(" ", "", regex=False)
+    data['rep'] = data['rep'].str.lower().str.replace(" ", "", regex=False)
 
     ## Description
-    data['Description'] = data['Description'].str.replace(r'^.*?(?:\|| {3,})', '', regex=True)
+    data['description'] = data['description'].str.replace(r'^.*?(?:\|| {3,})', '', regex=True)
 
-    data.to_excel('C:\\AI Merchant Transaction Matching\\LLM\\Transaction-Matching\\data\\Cleaned\\Cleaned_DATA.xlsx', index=False)
+    data.to_excel('C:\\AI Merchant Transaction Matching\\BERT_api\\data\\Cleaned\\Cleaned_DATA.xlsx', index=False)
+    print("cleaned data saved")
     return data
 
 
