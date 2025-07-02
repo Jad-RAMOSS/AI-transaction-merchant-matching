bcf1e412ae32eccd4dd95646f24ec2ca668557a8
Jad ElWahy <jad.sherif5@gmail.com>
2025-07-02 17:51:10 +0300

    changd cwd

--- predict/ecosystem.config.js
+++ predict/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ai-merchant-api',
-      cwd: '/app/Bank_Statement_Rep_Prediction/AI-transaction-merchant-matching/predict',
+      cwd: '/home/jad/Bank_Settlement/AI-transaction-merchant-matching/predict',
       script: 'uvicorn',
       args: 'main:app --host 0.0.0.0 --port 8000',
       interpreter: '/usr/bin/python3',
